2e14066026e08e89ee4f25a4838edf5164764667
oluwaseunmary <temitopeoluwaseun545@gmail.com>
2023-07-31 19:32:39 -0700

    0-print_listint

--- 0x13-more_singly_linked_lists/0-print_listint.c
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -13,11 +13,11 @@ size_t print_listint(const listint_t *h);
 {
 size_t numcount = 0;
 
-while(h != NULL)	
+while (h)
 {
- printf("%d\n", h->n);
- h = h->next;
- numcount++;
+printf("%d\n", h->n);
+h = h->next;
+numcount++;
 }
 return (numcount);
 }
