cdc0207f71b6638b6762a4b8b8d4594c8804802e
oluwaseunmary <temitopeoluwaseun535@gmail.com>
2023-09-05 00:50:19 -0700

    task2

--- 0x0A-argc_argv/1-args.c
+++ 0x0A-argc_argv/1-args.c
@@ -3,13 +3,13 @@
  * main -prints number of arguments
  * @argc: argument count
  * @argv: argument vector
- * Return: 0
+ * Return: (0)
  */
 
 int main(int argc, char *argv[])
 {
 	(void) argv;
 
-	printf("%d/n", argc - 1);
+	printf("%d\n", argc - 1);
 	return (0);
 }
