59f92907be8b59daf06cf12b1fd04a05b44325b3
oluwaseunmary <temitopeoluwaseun535@gmail.com>
2023-09-15 14:09:58 -0700

    printstr

---
+++ 0x10-variadic_functions/2-print_strings.c
@@ -0,0 +1,29 @@
+#include "variadic_functions.h"
+#include <stdio.h>
+
+/**
+ * print_strings - prints the string
+ * @separator: prints a separator between the strins
+ * @n: prints number
+ */
+
+void print_strings(const char *separator, const unsigned int n, ...)
+{
+	va_list word;
+	unsigned int i;
+	char *re;
+
+	va_start(word, n);
+	{
+		for (i = 0; i < n; i++)
+			re = va_arg(word, char *);
+		if (re == NULL)
+		printf(("nil"));
+	else
+		printf("%s", re);
+	if (i < n - 1)
+		printf("%s", separator);
+	}
+	va_end(word);
+	printf("\n");
+}
