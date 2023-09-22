fcd5f0c49395c96b9ebb1399bf766370291a28ab
oluwaseunmary <temitopeoluwaseun535@gmail.com>
2023-09-22 07:11:08 -0700

    first

--- 0x12-singly_linked_lists/100-first.c
+++ 0x12-singly_linked_lists/100-first.c
@@ -1,10 +1,14 @@
 #include <stdio.h>
 /**
  * main - check the code
+ * 
+ * Return: 0
+ *
  */
 int main (void)
 {
 	printf("You're beat! and yet, you must allow\n");
 	printf("I bore my house upon my back!\n");
+	return (0);
 }
 
