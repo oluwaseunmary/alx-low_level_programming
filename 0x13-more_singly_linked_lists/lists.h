6ab85524ce0075b778539ff350b96747911d27fb
oluwaseunmary <temitopeoluwaseun535@gmail.com>
2023-09-26 07:41:13 -0700

    task0

--- 0x13-more_singly_linked_lists/lists.h
+++ 0x13-more_singly_linked_lists/lists.h
@@ -1,5 +1,5 @@
 #ifndef LISTS_H
-#define LIST_H
+#define LISTS_H
 #include <stdio.h>
 #include <stdlib.h>
 
@@ -13,8 +13,8 @@
  */
 typedef struct listint_s
 {
-    int n;
-    struct listint_s *next;
+	int n;
+	struct listint_s *next;
 } listint_t;
 
 int _putchar(char c);
