9153bc17907eecea2e4290334cb6993bf95395d8
oluwaseunmary <temitopeoluwaseun535@gmail.com>
2023-09-26 07:20:08 -0700

    task0

--- 0x13-more_singly_linked_lists/0-print_listint.c
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -2,16 +2,18 @@
 /**
  * print_listint - the element of the listint list
  * @h: the pointer to the list
+ * 
  * Return: the number of nodes
  */
+
 size_t print_listint(const listint_t *h)
 {
 	size_t node = 0;
 
-	while (h->next != NULL)
-	{
+	if (h == NULL)
+		return (0); 
+	while (h != NULL) {
 		printf("%d\n", h->n);
-
 		h = h->next;
 		node++;
 	}
