cced2296276bfd902bed5d6875646c564cf6d5e3
oluwaseunmary <temitopeoluwaseun535@gmail.com>
2023-10-26 10:53:57 -0700

    task1

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -1,34 +1,37 @@
+#include <stdio.h>
 #include "main.h"
 /**
- * binary_to_uint - binary number to an unsigned int
- * @b: a pointer to the string
- * Return: the converted number or 0
+ * binary_to_uint - A function  that converts binary
+ * to an integers and decimal numbers
+ *
+ * @b: a pointer to the string of characters
+ *
+ * Return: the converted output / 0 if failed
  */
 unsigned int binary_to_uint(const char *b)
 {
-	unsigned int ui;
-	int len, base_two;
+	unsigned int stlen = 0;
+	size_t sum;
+	size_t i;
+	size_t j;
+	size_t raise = 1;
+	int base_2 = 2;
 
-	if (!b)
+	sum = 0;
+	if (b == NULL)
 		return (0);
-
-	ui = 0;
-
-	for (len = 0; b[len] != '\0'; len++)
-		;
-
-	for (len--, base_two = 1; len >= 0; len--, base_two *= 2)
+	for (stlen = 0; b[stlen] != '\0'; stlen++); 
+	if (stlen == 1 && (b[0] == '0' || b[0] == '1'))
+		return (b[0] - 48);
+	for (i = 0; b[i] != '\0'; i++)
 	{
-		if (b[len] != '0' && b[len] != '1')
-		{
-			return (0);
-		}
-
-		if (b[len] & 1)
-		{
-			ui += base_two;
-		}
+		if (b[i] != '0' && b[i] != '1')
+		return (0);
+		for (j = stlen - 1; j > 0; j--)
+		raise = raise * base_2;
+		sum = sum + (raise * (b[i] - 48));
+		stlen--;
+		raise = 1;
 	}
-
-	return (ui);
+		return (sum);
 }
