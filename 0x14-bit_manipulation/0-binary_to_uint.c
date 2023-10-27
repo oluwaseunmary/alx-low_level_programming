f1d0e6c5cc0c1f750466eed781d51db76d1e1d52
oluwaseunmary <temitopeoluwaseun535@gmail.com>
2023-10-27 04:19:39 -0700

    biary to uint

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -10,20 +10,21 @@
  */
 unsigned int binary_to_uint(const char *b)
 {
-	unsigned int stlen = 0;
-	size_t sum;
+	unsigned int stlen;
+	size_t sum = 0;
 	size_t i;
 	size_t j;
 	size_t raise = 1;
 	int base_2 = 2;
 
-	sum = 0;
 	if (b == NULL)
 		return (0);
 	for (stlen = 0; b[stlen] != '\0'; stlen++)
 		;
 	if (stlen == 1 && (b[0] == '0' || b[0] == '1'))
+	{
 		return (b[0] - 48);
+	}
 	for (i = 0; b[i] != '\0'; i++)
 	{
 		if (b[i] != '0' && b[i] != '1')
