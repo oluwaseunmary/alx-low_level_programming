391d4f6b8631b9244f889c96d98f4d5af8e9231c
oluwaseunmary <temitopeoluwaseun535@gmail.com>
2023-10-30 05:58:57 -0700

    task 0

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -1,31 +1,43 @@
 #include "main.h"
-
 /**
- * read_textfile - reads the text file
- * @filename: a pointer to the file name
- * @letters: he number of letters it should read and print
- * Return: 0
+ * read_textfile - reads a text file and print it to the POSIX stdoutput
+ * @filename: the name of the file to be read
+ * @letters: The number of the letter it shouls print and read
+ *
+ * Return: the actual number of letters it could read and print
  */
 ssize_t read_textfile(const char *filename, size_t letters)
 {
-	int filede;
-	ssize_t rdn, wrn;
+	int file_descriptor;
+	ssize_t num_read, num_written;
 	char *buffer;
 
-	if (filename == NULL)
+	if (!filename)
 		return (0);
-	filede = open(filename, O_RDONLY);
-	if (filede == -1)
+	file_descriptor = open(filename, O_RDONLY);
+	if (file_descriptor == -1)
 		return (0);
-
 	buffer = malloc(sizeof(char) * letters);
-
-	if (!buffer)
+	if (buffer == NULL)
+	{
+		close(file_descriptor);
 		return (0);
-
-	rdn = read(filede, buffer, letters);
-	wrn = write(STDOUT_FILENO, buffer, rdn);
+	}
+	num_read = read(file_descriptor, buffer, letters);
+	if (num_read == -1)
+	{
+		free(buffer);
+		close(file_descriptor);
+		return (0);
+	}
+	num_written = write(STDOUT_FILENO, buffer, num_read);
+	if (num_written == -1 || num_written != num_read)
+	{
+		free(buffer);
+		close(file_descriptor);
+			return (0);
+	}
 	free(buffer);
-	close(filede);
-	return (wrn);
+	close(file_descriptor);
+	return (num_read);
 }
