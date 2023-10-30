ee0ec575fec7099e8aa5d0751280146c85f060d6
oluwaseunmary <temitopeoluwaseun535@gmail.com>
2023-10-30 07:01:28 -0700

    task2

--- 0x15-file_io/2-append_text_to_file.c
+++ 0x15-file_io/2-append_text_to_file.c
@@ -12,7 +12,7 @@ int append_text_to_file(const char *filename, char *text_content)
 	int wrtstatus = 0;
 
 	if (!filename)
-		return (0);
+		return (-1);
 	if (text_content == NULL)
 		return (1);
 	file_descriptor = open(filename, O_WRONLY | O_APPEND);
